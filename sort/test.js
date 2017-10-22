af0963ece8bb90b66d824e75dddb12479639d331
ChiYanhui <yhchi95@gmail.com>
2017-10-22 22:27:23 +0800

    测试:增加shell排序

--- sort/test.js
+++ sort/test.js
@@ -2,17 +2,20 @@ var sort = {
     quicksort: require('./quicksort'),
     heapsort: require('./heapsort'),
     mergesort: require('./mergesort'),
+    shellsort: require('./shellsort'),
 };
 var t1 = 0,
     t2 = 0,
     t3 = 0,
+    t4 = 0,
     n = 0;
 function timeTest(size) {
     var arr1 = new Int16Array(size),
         arr2 = new Int16Array(size),
-        arr3 = new Int16Array(size);
+        arr3 = new Int16Array(size),
+        arr4 = new Int16Array(size);
     for (var i = 0; i < size; i++) {
-        arr1[i] = arr2[i] = arr3[i] = Math.random() * 2**16 - 2**15;
+        arr1[i] = arr2[i] = arr3[i] = arr4[i] = Math.random() * 2**16 - 2**15;
     }
     var t = new Date();
     sort.mergesort(arr1);
@@ -23,6 +26,9 @@ function timeTest(size) {
     t = new Date();
     sort.quicksort(arr3);
     t3 += new Date() - t;
+    t = new Date();
+    sort.shellsort(arr4);
+    t4 += new Date() - t;
     n++;
 }
 
@@ -32,7 +38,7 @@ function test() {
     }
     
     console.log('n: ' + n);
-    console.log('归并排序: ' + t1/n + '\n堆排序: ' + t2/n + '\n快速排序: ' + t3/n);
+    console.log('归并排序: ' + t1/n + '\n堆排序: ' + t2/n + '\n快速排序: ' + t3/n + '\n希尔排序: ' + t4/n);
     setTimeout(test, 1);
 }
 
