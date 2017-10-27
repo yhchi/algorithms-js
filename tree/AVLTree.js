cfa681df88970a50eebcd8a8d3fcb8590b872b98
ChiYanhui <yhchi95@gmail.com>
2017-10-27 22:25:01 +0800

    AVL树: 演示

--- tree/AVLTree.js
+++ tree/AVLTree.js
@@ -151,6 +151,28 @@ var AVLTree = (function () {
         return node;
     }
     
+    function LDR(node, fn) {
+        if (node.left)
+            LDR(node.left, fn);
+        fn(node.element, node);
+        if (node.right)
+            LDR(node.right, fn);
+    }
+    function DLR(node, fn) {
+        fn(node.element, node);
+        if (node.left)
+            DLR(node.left, fn);
+        if (node.right)
+            DLR(node.right, fn);
+    }
+    function LRD(node, fn) {
+        if (node.left)
+            LRD(node.left, fn);
+        if (node.right)
+            LRD(node.right, fn);
+        fn(node.element, node);
+    }
+    
     function AVLTree(arr, fn, fn2) {
         if (typeof arr === 'function') {
             fn2 = fn;
@@ -182,12 +204,28 @@ var AVLTree = (function () {
         delete: function(key, fn) {
             fn = fn || this.searchfn || this.insertfn;
             this.top = del(this.top, key, fn);
-        }
+        },
+        // 中序遍历
+        inorder: function(fn) {
+            if (this.top)
+                LDR(this.top, fn);
+        },
+        // 前序遍历 / 先根遍历
+        preorder: function(fn) {
+            if (this.top)
+                DLR(this.top, fn);
+        },
+        // 后序遍历 / 后根遍历
+        postorder: function(fn) {
+            if (this.top)
+                LRD(this.top, fn);
+        },
     };
     
     return AVLTree;
 })();
 
+/*
 var tree = new AVLTree([{
         code: 1,
         word: 'hello',
@@ -199,3 +237,34 @@ var tree = new AVLTree([{
         word: 'good',
     }], (a, b) => a.code - b.code, (k, e) => k - e.code);
 tree.search(3);
+*/
+
+function AVLTreeTest() {
+    var tree = new AVLTree(),
+        arr = [], str = '';
+    for (var i = 0; i < 5000; i++) {
+        var x = Math.floor(Math.random()*1000);
+        if (Math.random() < 0.7) {
+            tree.insert(x);
+            if (arr.indexOf(x) < 0)
+                arr.push(x);
+            str += 'insert('+x+');\n';
+        } else {
+            tree.delete(x);
+            var ind = arr.indexOf(x);
+            if (ind >= 0)
+                arr.splice(ind, 1);
+            str += 'del('+x+');\n';
+        }
+    }
+    var s1 = '', s2 = '';
+    tree.inorder((elem)=>{s1+=elem+','});
+    s2 = arr.sort((a,b)=>a-b).join(',')+',';
+    if (s1 !== s2) {
+        console.log(s1);
+        console.log(s2);
+        console.log(str);
+    } else {
+        console.log('正确');
+    }
+}
